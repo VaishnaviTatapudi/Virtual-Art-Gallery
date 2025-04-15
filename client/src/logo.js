54730970f63b2f48847b356fce7c4b70e219cd1b
VaishnaviTatapudi <21bd1a053vcseb@gmail.com>
2025-04-15 17:22:08 +0530

    Initial commit

---
+++ client/src/logo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import LogoImage from 'E:/miniproject/pinterest-clone-master/VArt/client/src/logo.webp'
+const Logo = () => {
+  return <img src={LogoImage} alt="VArt Logo" style={{ width: '100px', height: 'auto' }} />;
+};
+
+export default Logo;
